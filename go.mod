2b32b01f5e6333468c9a76db0d3fdf55d4816b05
douglas <douglas_sd0@outlook.com>
2023-06-20 20:17:33 -0300

    projeto_fc

---
+++ go.mod
@@ -0,0 +1,5 @@
+module projeto_fc
+
+go 1.20
+
+require github.com/google/uuid v1.3.0
---
+++ internal/market/entity/order_queue.go
@@ -0,0 +1,17 @@
+package entity
+
+type OrderQueue struct {
+	Orders []*Order
+}
+
+func (oq OrderQueue) Less(i, j int) bool{
+	return oq.Orders[i].Price < oq.Orders[j].Price
+}
+
+func (oq OrderQueue) Swap(i, j int){
+	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
+}
+
+func (oq OrderQueue) Len() int{
+	return len(oq.Orders)
+}
