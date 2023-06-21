84e51bf0d38197d0732e6b173b7acaeb50953ab6
douglas <douglas_sd0@outlook.com>
2023-06-21 00:07:04 -0300

    book_orders

---
+++ internal/market/entity/book.go
@@ -0,0 +1,50 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Order        []*Order
+	Transaction  []*Transaction
+	OrdersChan    chan *Order
+	OrdersChanOut chan *Order
+	Wg           *sync.WaitGroup
+}
+
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book{
+	return &Book{
+		Order:         []*Order{},
+		Transaction:   []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
+	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range b.OrdersChan {
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+			if sellOrders.Len() > 0 && sellOrders.Orders[0].Price <= order.Price {
+				sellOrder := sellOrders.Pop().(*Order)
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					b.AddTransaction(transaction , b.Wg)
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+				}
+			}
+		}
+	}
+
+}
+
+//video time 1:39:11
